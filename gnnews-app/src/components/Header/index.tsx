a9f38f81e98164ab769286ac5a2da49aa2b14be2
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-03-22 21:32:32 +0100

    added functional card/list button

--- gnnews-app/src/components/Header/index.tsx
+++ gnnews-app/src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import { Button, Col, Modal, Row } from 'react-bootstrap'
 import { IconContext } from 'react-icons'
-import { GrCircleInformation, GrList, GrAppsRounded } from 'react-icons/gr'
+import { AiOutlineAppstore, AiOutlineUnorderedList, AiOutlineInfoCircle } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import SideMenu from '../SideMenu'
+import type { RootState } from '../../store/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { turnOnCardsView, turnOnListView } from '../../features/news/newsSlice'
 
 const Header = () => {
 
@@ -12,6 +15,8 @@ const Header = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const isCardViewOn = useSelector((state: RootState) => state.news.newsCardsView)
+  const dispatch = useDispatch()
   
   return (
     <Row className='p-2'>
@@ -24,23 +29,26 @@ const Header = () => {
         </Link>
       </Col>
         <Col xs={ 1 } md="auto" className="align-self-center m-2" >
-          <Button variant="outline-secondary" className="rounded-circle p-1 border-0">
-            <IconContext.Provider value={{size: '24px'}}>
-              <GrAppsRounded/>
-            </IconContext.Provider> 
-          </Button>
+          {isCardViewOn 
+          ? 
+          (
+          <Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={(e) => dispatch(turnOnListView())}>
+          <IconContext.Provider value={{size: '36px', color: 'black'}}>
+            <AiOutlineUnorderedList/>
+          </IconContext.Provider> 
+        </Button>) 
+          : 
+          (<Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={(e) => dispatch(turnOnCardsView())}>
+          <IconContext.Provider value={{size: '36px', color: 'black'}}>
+            <AiOutlineAppstore/>
+          </IconContext.Provider> 
+        </Button>)
+        }
         </Col>
-        <Col xs={ 1 } md="auto" className="align-self-center m-2">
-          <Button variant="outline-secondary" className="rounded-circle p-1 border-0">
-            <IconContext.Provider value={{size: '24px'}}>
-              <GrList/>
-            </IconContext.Provider> 
-          </Button>
-        </Col>
-        <Col xs={ 1 } md="auto" className="align-self-center m-2">
+        <Col xs={ 2 } md="auto" className="align-self-center m-2">
           <Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={handleShow}>
-            <IconContext.Provider value={{size: '24px'}}>
-              <GrCircleInformation/>
+            <IconContext.Provider value={{size: '36px', color: 'black'}}>
+              <AiOutlineInfoCircle/>
             </IconContext.Provider> 
           </Button>
           <Modal show={show} onHide={handleClose}>
