cc2a9c068d9a392a96c888f9ec444b767a1c2c62
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-03-23 17:29:15 +0100

    fixed missing key atribute

--- gnnews-app/src/components/SideMenu/index.tsx
+++ gnnews-app/src/components/SideMenu/index.tsx
@@ -35,7 +35,7 @@ const SideMenu = () => {
         <Offcanvas.Body className="w-100">
           <ListGroup variant="flush">
             {countries.sort().map( (country) => (
-              <ListGroup.Item className='d-flex flex-row align-items-center' action onClick={() => handleClick(country.code)}>
+              <ListGroup.Item className='d-flex flex-row align-items-center' action onClick={() => handleClick(country.code)} key={country.code}>
                 <div className={`fib fi-${country.code.toLowerCase()} h-25 w-25`}>
                   <IconContext.Provider value={{size: '24px'}}>
                     <BsDot/>
