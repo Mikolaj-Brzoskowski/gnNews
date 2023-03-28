22e883a84e340e290b77c248e7e73a9f5dcc1bd5
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-03-28 20:47:20 +0200

    changed header components files structure

---
+++ gnnews-app/src/components/Header/HeaderButtons/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { Button, Col, Modal } from 'react-bootstrap'
+import { AiOutlineAppstore, AiOutlineUnorderedList, AiOutlineInfoCircle } from 'react-icons/ai'
+import { turnOnCardsView, turnOnListView } from '../../../features/newsSlice'
+import { Tooltip } from 'react-tooltip'
+import type { RootState } from '../../../store/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { IconContext } from 'react-icons'
+import { useTranslation } from 'react-i18next'
+
+const HeaderButtons = () => {
+
+    const isCardViewOn = useSelector((state: RootState) => state.news.newsCardsView)
+    const dispatch = useDispatch()
+    const { t } = useTranslation();
+
+    const [show, setShow] = useState<boolean>(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+        {/* card/list button */}
+            <Col xs={ 1 } md="auto" className="align-self-center m-2" >
+                {isCardViewOn 
+                ? 
+                (
+                <Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={(e) => dispatch(turnOnListView())} data-tooltip-id="button-1" data-tooltip-content={`${t('list')}`}>
+                    <IconContext.Provider value={{size: '36px', color: 'black'}}>
+                        <AiOutlineUnorderedList/>
+                    </IconContext.Provider>
+                    <Tooltip id="button-1" />
+                </Button>) 
+                    : 
+                (<Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={(e) => dispatch(turnOnCardsView())} data-tooltip-id="button-2" data-tooltip-content={`${t('cards')}`}>
+                    <IconContext.Provider value={{size: '36px', color: 'black'}}>
+                        <AiOutlineAppstore/>
+                    </IconContext.Provider> 
+                    <Tooltip id="button-2"/>
+                </Button>)
+                }
+            </Col>
+        {/* info button with modal popup */}
+            <Col xs={ 2 } md="auto" className="align-self-center m-2">
+                <Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={handleShow}>
+                    <IconContext.Provider value={{size: '36px', color: 'black'}}>
+                    <AiOutlineInfoCircle/>
+                    </IconContext.Provider> 
+                </Button>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{t('info')}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{t('like')} Cała tematyka projektu mi odpowiadała, dobrze się bawiłem robiąc w sumie pierwszy większy projekt w TypeScripcie. 
+                    Tłumaczenie strony było chyba najłatwiejsze, tym samym najprzyjemniejsze. Poza tym porównywałem sobie użycie Bootstrapa w przeciwieństwie do TailWinda, którego używam
+                    w tym momencie w innym swoim projekcie, dzięki temu wiem mniej więcej, którego frameworka lepiej używać i jakie oba z frameworków mają wady/zalety. </Modal.Body>
+                    <Modal.Body>{t('dislike')} Najwięcej problemów sprawiła mi paczka związana z flagami państw, których użyłem do wyświetlania w SideMenu. Będąc szczerym 
+                    nadal nie wiem w jaki sposób ona nadal działa, ale cieszę się, że działa w ogóle. Poza tym było parę dziwnych sytuacji z ustawianiem flexa/grida, ale to standard.</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="dark" onClick={handleClose}>{t('close')}</Button>
+                    </Modal.Footer>
+                </Modal>
+            </Col>
+        </>
+    )
+}
+
+export default HeaderButtons
--- gnnews-app/src/components/Header/index.tsx
+++ gnnews-app/src/components/Header/index.tsx
@@ -1,74 +1,44 @@
-import React, { useState } from 'react'
-import { Button, Col, Modal, Row } from 'react-bootstrap'
-import { IconContext } from 'react-icons'
-import { AiOutlineAppstore, AiOutlineUnorderedList, AiOutlineInfoCircle } from 'react-icons/ai'
+import React from 'react'
+import { Button, Col, Nav, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import SideMenu from '../SideMenu'
-import type { RootState } from '../../store/store'
-import { useSelector, useDispatch } from 'react-redux'
-import { turnOnCardsView, turnOnListView } from '../../features/newsSlice'
-import { Tooltip } from 'react-tooltip'
 import { useTranslation } from 'react-i18next';
+import HeaderButtons from './HeaderButtons'
 
 const Header = () => {
 
-  const [show, setShow] = useState<boolean>(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  const isCardViewOn = useSelector((state: RootState) => state.news.newsCardsView)
-  const dispatch = useDispatch()
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   
   return (
     <Row className='p-2'>
       <Col xs="auto" className="align-self-center">
         <SideMenu/>
       </Col>
-      <Col className="mt-2">
+      <Col xs="auto" className="mt-2">
         <Link to='/' className="text-decoration-none text-color-black text-reset">
           <h1 className="text-sm-left mt-1">gnNews</h1>
         </Link>
       </Col>
-        <Col xs={ 1 } md="auto" className="align-self-center m-2" >
-          {isCardViewOn 
-          ? 
-          (
-          <Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={(e) => dispatch(turnOnListView())} data-tooltip-id="button-1" data-tooltip-content={`${t('list')}`}>
-          <IconContext.Provider value={{size: '36px', color: 'black'}}>
-            <AiOutlineUnorderedList/>
-          </IconContext.Provider>
-          <Tooltip id="button-1" />
-        </Button>) 
-          : 
-          (<Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={(e) => dispatch(turnOnCardsView())} data-tooltip-id="button-2" data-tooltip-content={`${t('cards')}`}>
-          <IconContext.Provider value={{size: '36px', color: 'black'}}>
-            <AiOutlineAppstore/>
-          </IconContext.Provider> 
-          <Tooltip id="button-2"/>
-        </Button>)
-        }
-        </Col>
-        <Col xs={ 2 } md="auto" className="align-self-center m-2">
-          <Button variant="outline-secondary" className="rounded-circle p-1 border-0" onClick={handleShow}>
-            <IconContext.Provider value={{size: '36px', color: 'black'}}>
-              <AiOutlineInfoCircle/>
-            </IconContext.Provider> 
-          </Button>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>{t('info')}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>{t('like')} </Modal.Body>
-            <Modal.Body>{t('dislike')} </Modal.Body>
-            <Modal.Footer>
-              <Button variant="dark" onClick={handleClose}>
-              {t('close')}
+      <Col xs={1} md={4}/>
+        <HeaderButtons/>
+    {/* Laungage switch */}
+        <Col xs="auto" className="mt-2 p-2">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link disabled className='text-black'>{t('language')}</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+              <Button variant="outline-light" className="p-0 border-0">
+                <Nav.Link onClick={() => i18n.changeLanguage("en")} className='text-black'>{t('en')}</Nav.Link>
               </Button>
-            </Modal.Footer>
-          </Modal>
-        </Col>
+              </Nav.Item>
+              <Nav.Item>
+              <Button variant="outline-light" className="p-0 border-0">
+                <Nav.Link onClick={() => i18n.changeLanguage("pl")} className='text-black'>{t('pl')}</Nav.Link>
+              </Button>
+              </Nav.Item>
+            </Nav>
+          </Col>
     </Row>
   )
 }
